ef7adfa006aa7a4f5663db492610cff49f82aca7
deployable <matt@deployable.co>
2014-11-20 07:54:13 +0000

    Make PUB/SUB topic generic add in zmq to gemspec

--- lib/deployable/zmq/pubsub.rb
+++ lib/deployable/zmq/pubsub.rb
@@ -8,28 +8,35 @@ module Deployable; module Zmq
 DEFAULT_BIND_PORT    = 5563
 DEFAULT_BIND_ADDRESS = '*'
 
-class RequestPublish
+
+class Publish
 
   @port    = DEFAULT_BIND_PORT
   @address = DEFAULT_BIND_ADDRESS
 
-  def initialize
+  def initialize options = {}
     @context   = ZMQ::Context.new
     @publisher = @context.socket ZMQ::PUB
-    @port      = __class_ivg 'port'
-    @address   = __class_ivg 'address'
-    @publisher.bind "tcp://#{address}:#{port}"
+
+    @port      = options.fetch :port, __class_ivg( 'port' )
+    @port      = DEFAULT_BIND_PORT if @port.nil?
+
+    @address   = options.fetch :address, __class_ivg( 'address' )
+    @address   = DEFAULT_BIND_ADDRESS if @address.nil?
+
+    uri = "tcp://#{@address}:#{@port}"
+    unless rc = @publisher.bind( uri ) == 0
+      raise "zmq bind failed [#{rc}] [#{uri}]"; end
+    puts "zmq pusblish on [#{uri}]"
   end
 
-  def send request, icap_code, response_code
-    @publisher.send_string 'REQ_COMP', ZMQ::SNDMORE
-    @publisher.send_string request, ZMQ::SNDMORE
-    @publisher.send_string icap_code, ZMQ::SNDMORE
-    @publisher.send_string response_code
+  def send_string label, message
+    @publisher.send_string label, ZMQ::SNDMORE
+    @publisher.send_string message
   end
 
-  def send_arr array
-    @publisher.send_string 'REQ_COMP', ZMQ::SNDMORE
+  def send_arr label, array
+    @publisher.send_string label, ZMQ::SNDMORE
     limit = array.length - 1
     i=0; while i < limit do
       @publisher.send_string array[i], ZMQ::SNDMORE
@@ -38,10 +45,10 @@ class RequestPublish
     @publisher.send_string array[i]
   end
 
-  def send_json obj
+  def send_json label, obj
     # parse before send in case of issues
     message = obj.to_json
-    @publisher.send_string 'REQ_COMP', ZMQ::SNDMORE
+    @publisher.send_string label, ZMQ::SNDMORE
     @publisher.send_string message
   end
 
@@ -53,19 +60,30 @@ end
 
 
 
-class RequestSubscribe
+class Subscribe
 
-  @port    = BIND_PORT
-  @address = BIND_ADDRESS
+  @port    = DEFAULT_BIND_PORT
+  @address = 'localhost'
   
-  def initialize address = nil
+  def initialize options = {}
     @context    = ZMQ::Context.new
     @subscriber = @context.socket ZMQ::SUB
-    @port       = __class_ivg 'port'
-    @address    = address
-    @address    = __class_ivg 'address' if @address.nil?
-    @subscriber.connect "tcp://#{@address}:#{port}"
-    @subscriber.setsockopt ZMQ::SUBSCRIBE, 'REQ_COMP'
+
+    @port       = options.fetch :port, __class_ivg('port')
+    @port       = DEFAULT_BIND_PORT if @port.nil?
+
+    @address    = options.fetch :options, __class_ivg('address')
+    raise "No valid address [#{@address}]" if @address.nil? or @address == '*'
+
+    url = "tcp://#{@address}:#{@port}"
+
+    unless rc = @subscriber.connect( url ) == 0 
+      raise "Failed to connect to [#{url}] [#{rc}]"; end
+
+    if options.fetch( :subscribe, nil ) != nil
+      unless rc = @subscriber.setsockopt( ZMQ::SUBSCRIBE, 'REQ_COMP' ) == 0 
+        raise "Failed to subscribe to [#{url}] [#{rc}]"; end
+    end
   end
 
   def receive
